3dc0275da4289b2368f310247c08d2c7746a82f4
wyatthlang <wyatthlang@gmail.com>
2025-11-07 18:49:48 -0500

    initial

---
+++ src/main/java/kyx/hackathon/merchandize/MerchandizeApplication.java
@@ -0,0 +1,13 @@
+package kyx.hackathon.merchandize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchandizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchandizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyx/hackathon/merchandize/MerchandizeApplicationTests.java
@@ -0,0 +1,13 @@
+package kyx.hackathon.merchandize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchandizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
