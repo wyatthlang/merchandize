fa347bc0f7d3ac54b4c3c74a835463cef071fd9a
wyatthlang <wyatthlang@gmail.com>
2025-11-07 22:35:54 -0500

    merge

--- src/main/java/kyx/hackathon/merchandize/controller/MerchController.java
+++ src/main/java/kyx/hackathon/merchandize/controller/MerchController.java
@@ -24,8 +24,6 @@ public class MerchController {
         dir.mkdirs();
       }
 
-      
-
       ProcessBuilder processBuilder = new ProcessBuilder(
         "yt-dlp",
         "-f mp4",
---
+++ src/main/java/kyx/hackathon/merchandize/model/ImageGenRequest.java
@@ -0,0 +1,7 @@
+package kyx.hackathon.merchandize.model;
+
+public class ImageGenRequest {
+    
+    
+
+}
